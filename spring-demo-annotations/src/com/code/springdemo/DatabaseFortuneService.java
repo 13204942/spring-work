b9bfd16b7b98962343ccdc8b5e2a7dd1c21df831
13204942 <wfyj2010@gmail.com>
2017-05-20 19:14:32 +0100

    Field injection practice with Autowired and load values from files

--- spring-demo-annotations/src/com/code/springdemo/DatabaseFortuneService.java
+++ spring-demo-annotations/src/com/code/springdemo/DatabaseFortuneService.java
@@ -1,13 +1,38 @@
 package com.code.springdemo;
 
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseFortuneService implements FortuneService {
 
+	// load values from file
+	@Value("${db.username}")
+	private String username;
+	
+	@Value("${db.password}")
+	private String password;
+	
+	@Value("${db.port}")
+	private String port;
+	
+	// create an array of strings
+	private String[] data = new String[3];
+	
+	// create a random number generator
+	private Random myRandom = new Random();
+	
 	@Override
 	public String getFortune() {
-		return null;
+		data[0] = username;
+		data[1] = password;
+		data[2] = port;
+		
+		int index =  myRandom.nextInt(data.length);
+		String theFortune = data[index];
+		return theFortune;
 	}
 
 }
